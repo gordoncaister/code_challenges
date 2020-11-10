ac69dd255eeeb191427d2181a874ef8ada6586e7
Gordon Caister <gordoncaister@gmail.com>
2020-11-09 18:33:36 -0600

    added new challenge

---
+++ Python/Nested_Lists.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/nested-list/problem
+        # 5
+        # Harry
+        # 37.21
+        # Berry
+        # 37.21
+        # Tina
+        # 37.2
+        # Akriti
+        # 41
+        # Harsh
+        # 39
+        # Berry
+        # Harry
+#   python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+# Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
